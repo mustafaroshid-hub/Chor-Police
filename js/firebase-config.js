fb2fb9ab069b535ac786989d49f40cc20e67fe23
mustafaroshid-hub <mustafaroshid@gmail.com>
2026-08-29 15:40:11 +0600

    Delete js/firebase-config.js

--- js/firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAnPkCPNj0mURs4QtaoEoDru5FMc9Ugjpw",
-  authDomain: "chor-police-d6932.firebaseapp.com",
-  projectId: "chor-police-d6932",
-  storageBucket: "chor-police-d6932.firebasestorage.app",
-  messagingSenderId: "182044876485",
-  appId: "1:182044876485:web:a2a3925b71c35a6d33daac",
-};
